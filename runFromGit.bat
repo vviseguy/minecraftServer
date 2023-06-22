b2b740dd1676b0627925eb1046b99eeb3f8f7411
Jacob Wise <jacob@wisefam.net>
2023-06-22 12:55:56 -0700

    Gameplay hosted by Jacob on Thu 06/22/2023

--- runFromGit.bat
+++ runFromGit.bat
@@ -2,14 +2,14 @@
 
 echo Pulling updates from GitHub
 git pull
-echo
+echo 
 
-start cmd -k run.bat
-pause
-echo
+start cmd -c ./run.bat && {
+echo 
 echo Loading updates to Github
-echo
+echo 
 git add .
 git commit -m "Gameplay hosted by %USERNAME% on %DATE%"
 git push
 pause
+}
